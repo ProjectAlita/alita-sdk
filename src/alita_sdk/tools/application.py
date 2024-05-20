6600168fd8ff29016a03cfac57764f1b363bbad6
arozumenko <artyom.rozumenko@gmail.com>
2024-05-20 14:40:33 +0300

    fixing apps

--- src/alita_sdk/tools/application.py
+++ src/alita_sdk/tools/application.py
@@ -1,9 +1,13 @@
 from langchain.tools import BaseTool
 from langchain.pydantic_v1 import BaseModel, Field
 from typing import Any
-from langchain.pydantic_v1 import create_model
+from pydantic import create_model
+from pydantic.fields import FieldInfo
 
-applicationToolSchema = create_model("applicatrionSchema", task = (str, None), chat_history = (str, None))
+applicationToolSchema = create_model(
+    "applicatrionSchema", task = (str, FieldInfo(description="Task for Application")), 
+    chat_history = (str, FieldInfo(description="Chat History relevant for Application"))
+)
 
 class Application(BaseTool):
     name: str
