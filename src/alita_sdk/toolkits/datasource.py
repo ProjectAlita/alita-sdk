4aee2f2cc23a6263e60785d0dc5f7b6271a842e2
mikalai_biazruchka <mikalai_biazruchka@epam.com>
2025-03-14 15:26:23 +0300

    Update datasource naming

--- src/alita_sdk/toolkits/datasource.py
+++ src/alita_sdk/toolkits/datasource.py
@@ -1,8 +1,10 @@
 from typing import List, Any
 from pydantic import create_model, BaseModel, Field
 from langchain_community.agent_toolkits.base import BaseToolkit
-from langchain_core.tools import BaseTool
+from langchain_core.tools import BaseTool, ToolException
 from ..tools.datasource import DatasourcePredict, DatasourceSearch, datasourceToolSchema
+from alita_tools.utils import clean_string
+
 
 class DatasourcesToolkit(BaseToolkit):
     tools: List[BaseTool] = []
@@ -21,14 +23,17 @@ class DatasourcesToolkit(BaseToolkit):
         tools = []
         for datasource_id in datasource_ids:
             datasource = client.datasource(datasource_id)
+            ds_name = clean_string(datasource.name)
+            if len(ds_name) == 0:
+                raise ToolException(f'Datasource with id {datasource_id} has incorrect name (i.e. special characters, etc.)')
             if len(selected_tools) == 0 or 'chat' in selected_tools:
-                tools.append(DatasourcePredict(name=f'{datasource.name}Predict', 
+                tools.append(DatasourcePredict(name=f'{ds_name}Predict',
                                             description=f'Search and summarize. {datasource.description}',
                                             datasource=datasource, 
                                             args_schema=datasourceToolSchema,
                                             return_type='str'))
             if len(selected_tools) == 0 or 'search' in selected_tools:
-                tools.append(DatasourceSearch(name=f'{datasource.name}Search', 
+                tools.append(DatasourceSearch(name=f'{ds_name}Search',
                                                 description=f'Search return results. {datasource.description}',
                                                 datasource=datasource, 
                                                 args_schema=datasourceToolSchema,
