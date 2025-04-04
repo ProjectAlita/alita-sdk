82b2a740e968ebe76f53d13d75ce104b8d92cab6
mikalai_biazruchka <mikalai_biazruchka@epam.com>
2025-04-04 12:00:37 +0300

    Update datasource toolkits naming

--- src/alita_sdk/toolkits/datasource.py
+++ src/alita_sdk/toolkits/datasource.py
@@ -1,9 +1,9 @@
-from typing import List, Any
+from typing import List, Any, Optional
 from pydantic import create_model, BaseModel, Field
 from langchain_community.agent_toolkits.base import BaseToolkit
 from langchain_core.tools import BaseTool, ToolException
 from ..tools.datasource import DatasourcePredict, DatasourceSearch, datasourceToolSchema
-from alita_tools.utils import clean_string
+from alita_tools.utils import clean_string, TOOLKIT_SPLITTER
 
 
 class DatasourcesToolkit(BaseToolkit):
@@ -15,25 +15,26 @@ class DatasourcesToolkit(BaseToolkit):
             "datasource",
             # client = (Any, FieldInfo(description="Client object", required=True, autopopulate=True)),
             datasource_ids = (list, Field(description="List of datasource ids", alias="datasource_id")),
-            selected_tools = (list, Field(description="List of selected tools", default=['chat', 'search']))
+            selected_tools = (list, Field(description="List of selected tools", default=['chat', 'search'])),
         )
     
     @classmethod
-    def get_toolkit(cls, client: Any, datasource_ids: list[int], selected_tools: list[str] = []):
+    def get_toolkit(cls, client: Any, datasource_ids: list[int], toolkit_name: Optional[str] = None, selected_tools: list[str] = []):
         tools = []
+        prefix = clean_string(toolkit_name) + TOOLKIT_SPLITTER if toolkit_name else ''
         for datasource_id in datasource_ids:
             datasource = client.datasource(datasource_id)
             ds_name = clean_string(datasource.name)
             if len(ds_name) == 0:
                 raise ToolException(f'Datasource with id {datasource_id} has incorrect name (i.e. special characters, etc.)')
             if len(selected_tools) == 0 or 'chat' in selected_tools:
-                tools.append(DatasourcePredict(name=f'{ds_name}Predict',
+                tools.append(DatasourcePredict(name=f'{prefix}chat',
                                             description=f'Search and summarize. {datasource.description}',
                                             datasource=datasource, 
                                             args_schema=datasourceToolSchema,
                                             return_type='str'))
             if len(selected_tools) == 0 or 'search' in selected_tools:
-                tools.append(DatasourceSearch(name=f'{ds_name}Search',
+                tools.append(DatasourceSearch(name=f'{prefix}search',
                                                 description=f'Search return results. {datasource.description}',
                                                 datasource=datasource, 
                                                 args_schema=datasourceToolSchema,
