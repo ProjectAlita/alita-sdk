3c3bf15c4e429a7d69c53d86e62977d92e68a195
mikalai_biazruchka <mikalai_biazruchka@epam.com>
2025-06-24 17:50:31 +0300

    add dependencies, introduce store_manager

---
+++ alita_sdk/tools/memory/__init__.py
@@ -0,0 +1,40 @@
+from typing import Optional, List
+
+from langchain_core.tools import BaseToolkit, BaseTool
+from langgraph.store.postgres import PostgresStore
+from langmem import create_manage_memory_tool, create_search_memory_tool
+from pydantic import create_model, BaseModel, ConfigDict, Field, SecretStr
+
+name = "memory"
+
+class MemoryToolkit(BaseToolkit):
+    tools: List[BaseTool] = []
+
+
+    @staticmethod
+    def toolkit_config_schema() -> BaseModel:
+        return create_model(
+            name,
+            namespace=(str, Field(description="Memory namespace", json_schema_extra={'toolkit_name': True})),
+            connection_string=(Optional[SecretStr], Field(description="Connection string for vectorstore",
+                                                          default=None,
+                                                          json_schema_extra={'secret': True})),
+            __config__=ConfigDict(json_schema_extra={
+                'metadata': {
+                    "label": "Memory",
+                    "icon_url": "jira-icon.svg",
+                    "categories": ["other"],
+                    "extra_categories": ["long-term memory", "langmem"],
+                }
+            })
+        )
+
+    @classmethod
+    def get_toolkit(cls, namespace: str, store: PostgresStore, **kwargs):
+        return cls(tools=[
+            create_manage_memory_tool(namespace=namespace, store=store),
+            create_search_memory_tool(namespace=namespace, store=store)
+        ])
+
+    def get_tools(self):
+        return self.tools
