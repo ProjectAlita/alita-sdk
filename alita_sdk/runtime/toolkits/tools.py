69629819b6c732c7d628215b19c64a1b8603c586
mikalai_biazruchka <mikalai_biazruchka@epam.com>
2025-06-26 11:01:17 +0300

    revert changes

--- alita_sdk/runtime/toolkits/tools.py
+++ alita_sdk/runtime/toolkits/tools.py
@@ -67,6 +67,7 @@ def get_tools(tools_list: list, alita_client, llm, memory_store: BaseStore = Non
                 selected_tools=[],
                 llm=llm
             ))
+            # move on tools level
         # elif tool['type'] == 'memory':
         #     if memory_store is None:
         #         raise ToolException(f"Memory store is not provided for memory tool: {tool['name']}")
