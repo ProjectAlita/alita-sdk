27d00d37eebaca3ff6bcb278c502a0cb7c8e6f63
Aliaksei_Breilian <aliaksei_breilian@epam.com>
2025-08-06 15:23:50 +0300

    fix ado-mcp tools

--- src/alita_sdk/tools/mcp_server_tool.py
+++ src/alita_sdk/tools/mcp_server_tool.py
@@ -22,7 +22,13 @@ class McpServerTool(BaseTool):
     def create_pydantic_model_from_schema(schema: dict):
         fields = {}
         for field_name, field_info in schema['properties'].items():
-            field_type = field_info['type']
+            field_type = None
+
+            if 'type' in field_info:
+                field_type = field_info['type']
+            elif 'allOf' in field_info or 'anyOf' in field_info:# ADO-MCP wit_update_work_item, testplan_add_test_cases_to_suite
+                field_type = 'array'
+            
             field_description = field_info.get('description', '')
             if field_type == 'string':
                 if 'enum' in field_info:
@@ -35,6 +41,22 @@ class McpServerTool(BaseTool):
                 field_type = float
             elif field_type == 'boolean':
                 field_type = bool
+            elif field_type == 'object':#Dict[str, Any] - for tool start_browser in selenium mcp
+                nested_model = McpServerTool.create_pydantic_model_from_schema(field_info) if 'properties' in field_info else dict# for tool build_run_build in ADO-MCP
+                field_type = nested_model
+            elif field_type == 'array':
+                item_schema = field_info['items'] if 'items' in field_info else (field_info.get('allOf', field_info.get('anyOf', [{'type': None}]))[-1])# ADO-MCP wit_update_work_item, testplan_add_test_cases_to_suite
+                item_type = McpServerTool.create_pydantic_model_from_schema(item_schema) if item_schema['type'] == 'object' else (
+                    str if item_schema['type'] == 'string' else # support enum
+                    int if item_schema['type'] == 'integer' else
+                    float if item_schema['type'] == 'number' else
+                    bool if item_schema['type'] == 'boolean' else
+                    list if item_schema['type'] == 'array' else# ADO-MCP testplan_add_test_cases_to_suite
+                    None
+                )
+                if item_type is None:
+                    raise ValueError(f"Unsupported array item type: {item_schema['type']}")
+                field_type = list[item_type]
             else:
                 raise ValueError(f"Unsupported field type: {field_type}")
 
