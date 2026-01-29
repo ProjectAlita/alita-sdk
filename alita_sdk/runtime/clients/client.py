014af11020dcb70347d2a2ee68f656c87fa3e5da
MikalaiB <58071282+MikalaiB@users.noreply.github.com>
2026-01-29 12:27:46 +0300

    Update base_app_url to use api_v2_path

--- alita_sdk/runtime/clients/client.py
+++ alita_sdk/runtime/clients/client.py
@@ -93,7 +93,7 @@ class AlitaClient:
         if api_extra_headers is not None:
             self.headers.update(api_extra_headers)
         self.predict_url = f"{self.base_url}{self.api_path}/prompt_lib/predict/prompt_lib/{self.project_id}"
-        self.base_app_url = f"{self.base_url}{self.api_path}/applications/application/prompt_lib/"
+        self.base_app_url = f"{self.base_url}{self.api_v2_path}/applications/application/prompt_lib/"
         self.base_public_app_url = f"{self.base_url}{self.api_path}/applications/public_application/prompt_lib/"
         self.app = f"{self.base_app_url}{self.project_id}"
         self.mcp_tools_list = f"{self.base_url}{self.api_path}/mcp_sse/tools_list/{self.project_id}"
