a571308989fa8c530b43eb62de6d00a1695fa4e7
mikalai_biazruchka <mikalai_biazruchka@epam.com>
2026-02-09 21:31:49 +0300

    feature(xxxx): error handling
    add missing reference to exception instance

--- alita_sdk/runtime/tools/function.py
+++ alita_sdk/runtime/tools/function.py
@@ -214,8 +214,8 @@ alita_state = json.loads(state_json)
                     return messages_dict
                 else:
                     return { self.output_variables[0]: tool_result }
-        # save whole error message to tool's output
-        except Exception:
+        # save the whole error message to the tool's output
+        except Exception as e:
             return {"messages": [
                 {"role": "assistant", "content": f"""Tool input to the {self.tool.name} with value {func_args} raised Exception. 
                         \n\nTool schema is {safe_serialize(params)}. \n\n Details: {e}"""}]}
